93982a9203accc3438f9c115d18448332289eeba
Kay Kim <kimjh@bawi.org>
2021-11-10 13:41:45 +0900

    Feat: Add CSP header to next.config.js (#16)
    - Add X-XSS-Protection header
    - Add Content Security Policy header

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
 // @ts-check
 
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; base-uri 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval'; object-src 'self'; img-src 'self' data:",
+  },
+];
+
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-    /* config options here */
+  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
