18ad5822fe4b7c521ff0dfdb609c86bef1eb7071
Kay Kim <kimjh@bawi.org>
2021-11-14 17:02:56 +0900

    fix: Add google fonts to CSR header

--- next.config.js
+++ next.config.js
@@ -7,8 +7,15 @@ const securityHeaders = [
   },
   {
     key: 'Content-Security-Policy',
-    value:
-      "default-src 'self'; base-uri 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval'; object-src 'self'; img-src 'self' data:",
+    value: [
+      `default-src 'self'`,
+      `base-uri 'self'`,
+      `style-src 'self' fonts.googleapis.com 'unsafe-inline'`,
+      `font-src 'self' fonts.gstatic.com`,
+      `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
+      `object-src 'self'`,
+      `img-src 'self' data:`,
+    ].join(';'),
   },
 ];
 
