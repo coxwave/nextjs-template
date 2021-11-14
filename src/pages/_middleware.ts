750e395536a837d85ff8383a99d3b93fb91e1880
Kay Kim <kimjh@bawi.org>
2021-11-14 17:02:31 +0900

    fix: Change rewrite to redirect for /docs

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -5,6 +5,6 @@ import type { NextFetchEvent, NextRequest } from 'next/server';
 // this _middleware rewrites `/docs` to `/docs/index.html`
 export function middleware(req: NextRequest, _ev: NextFetchEvent) {
   if (req.url === '/docs') {
-    return NextResponse.rewrite('/docs/index.html');
+    return NextResponse.redirect('/docs/index.html');
   }
 }
