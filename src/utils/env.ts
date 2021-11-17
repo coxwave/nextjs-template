727f26069b3e86bb0479fba64310a698f5d4f783
Kay Kim <kimjh@bawi.org>
2021-11-17 11:17:17 +0900

    fix: Fix export orders for utils/env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,17 @@
 const ENVs = ['JWT_SECRET', 'HASHIDS_KEY', 'MONGODB_URI', 'MONGODB_NAME'] as const;
 
+export const isTest: () => boolean = () => {
+  return process.env.NODE_ENV === 'test';
+};
+
+export const isDev: () => boolean = () => {
+  return process.env.NODE_ENV === 'development';
+};
+
+export const isProd: () => boolean = () => {
+  return process.env.NODE_ENV === 'production';
+};
+
 export const getEnv: (name: typeof ENVs[number]) => string = (name: string) => {
   const val = process.env[name];
   if (val !== undefined) {
@@ -17,15 +29,3 @@ export const JWT_SECRET = getEnv('JWT_SECRET');
 export const HASHIDS_KEY = getEnv('HASHIDS_KEY');
 export const MONGODB_URI = getEnv('MONGODB_URI');
 export const MONGODB_NAME = getEnv('MONGODB_NAME');
-
-export const isTest: () => boolean = () => {
-  return process.env.NODE_ENV === 'test';
-};
-
-export const isDev: () => boolean = () => {
-  return process.env.NODE_ENV === 'development';
-};
-
-export const isProd: () => boolean = () => {
-  return process.env.NODE_ENV === 'production';
-};
