31a842a4d07d23cdce1e3b61d2e309959383af74
Kay Kim <kimjh@bawi.org>
2021-12-10 10:06:07 +0900

    feat: Remove dependencies from tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [require('@tailwindcss/forms')],
 };
