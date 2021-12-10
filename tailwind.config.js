f813055d008507b09fc881a240069dcb1120699b
Kay Kim <kimjh@bawi.org>
2021-12-10 13:05:09 +0900

    feat: Move to tailwindcss v3 (#49)
    closes #17
    
    - Remove dependencies from tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/frontend/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/frontend/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -10,15 +7,7 @@ module.exports = {
         // 'Inter' fontFamily was imported in _document.tsx
         inter: "'Inter'",
       },
-      colors: {
-        // You can add more colors here.
-        sky: colors.sky,
-      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [require('@tailwindcss/forms')],
 };
