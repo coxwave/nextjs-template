7d577c9b5286c64319c3de3be4d8734a8478bbc4
Kay Kim <kimjh@bawi.org>
2021-12-10 17:47:32 +0900

    hotfix: Optimize for tailwindcss@3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/frontend/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
