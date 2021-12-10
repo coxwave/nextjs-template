b243d1fd2a21444c579fd8a7ca2d0d583e673e4f
Kay Kim <kimjh@bawi.org>
2021-12-10 09:59:27 +0900

    feat: Move to tailwindcss v3
    closes #17

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/frontend/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/frontend/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         // 'Inter' fontFamily was imported in _document.tsx
         inter: "'Inter'",
-      },
-      colors: {
-        // You can add more colors here.
-        sky: colors.sky,
       },
     },
   },
